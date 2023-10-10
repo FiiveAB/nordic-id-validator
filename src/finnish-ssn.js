ca1b0dc25af9a5ff9874027e07d087e5d68459f4
Lucas Rosvall <lucasrosvall@gmail.com>
2023-10-10 15:01:33 +0200

    support for finnish ssn

--- src/finnish-ssn.js
+++ src/finnish-ssn.js
@@ -1,14 +1,23 @@
-import { testDate, teshLuhn } from "./general";
+import { testDate } from "./general";
 
 const checksumTable = '0123456789ABCDEFHJKLMNPRSTUVWXY'.split('')
+const SSN_REGEX = /^(0[1-9]|[12]\d|3[01])(0[1-9]|1[0-2])([5-9]\d\+|\d\d[-|U-Y]|[012]\d[A-F])\d{3}[\dA-Z]$/
 
 export default function fiPersonalNumber(number) {
+    if (!SSN_REGEX.test(ssn)) {
+        return false
+    }
+
+    const rollingId = ssn.substring(7, 10)
+    const checksum = ssn.substring(10, 11)
+    const checksumBase = parseInt(ssn.substring(0, 6) + rollingId, 10)
+
     return (
         // Length must be 10 digits
-        number.length === 10 &&
+        checksum === checksumTable[checksumBase % 31] &&
         // Validate the date of birth
         testDate(number) &&
         // Validate the check digit (Luhn algorithm)
-        teshLuhn(number)
+        checksum === checksumTable[checksumBase % 31]
     )
 }
